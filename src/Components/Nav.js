7c80ba9471e08863d92518fcd5db71de4ade3801
Samuel-Kwon <samuel.seonje.kwon@gmail.com>
2020-08-24 19:12:13 -0400

    Nav href fixed

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,8 +6,8 @@ export class Nav extends React.Component {
     return (
       <div className="nav">
         <div className="nav-text">
-          <a href="/view">View</a>
-          <a href="/upload">Upload</a>
+          <a href="/my-music-joureny/#/view">View</a>
+          <a href="/my-music-joureny/#/upload">Upload</a>
         </div>
       </div>
     );
