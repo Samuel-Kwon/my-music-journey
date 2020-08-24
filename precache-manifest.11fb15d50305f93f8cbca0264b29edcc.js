c86fa65f436196226352743b68509a77c949fed9
Samuel-Kwon <samuel.seonje.kwon@gmail.com>
2020-08-24 17:29:31 -0400

    Updates

---
+++ precache-manifest.11fb15d50305f93f8cbca0264b29edcc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8919d5115f849dc794113c75ef6a75a0",
+    "url": "/my-music-journey/index.html"
+  },
+  {
+    "revision": "d11471ba354f4f470777",
+    "url": "/my-music-journey/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "3964e149e695d67bf1f1",
+    "url": "/my-music-journey/static/css/main.92f15bff.chunk.css"
+  },
+  {
+    "revision": "d11471ba354f4f470777",
+    "url": "/my-music-journey/static/js/2.e6f8c0a1.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/my-music-journey/static/js/2.e6f8c0a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3964e149e695d67bf1f1",
+    "url": "/my-music-journey/static/js/main.f6d8ae54.chunk.js"
+  },
+  {
+    "revision": "d82fbd1003fcd83469b5",
+    "url": "/my-music-journey/static/js/runtime-main.6f5ec919.js"
+  },
+  {
+    "revision": "1c2a174024e20287ae79b1220edc92f8",
+    "url": "/my-music-journey/static/media/Landing.1c2a1740.jpg"
+  }
+]);
