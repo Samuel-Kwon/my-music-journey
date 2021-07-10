01840d86da68b09c0bfdf47505e5806305f9543d
unknown <samuel.seonje,kwon@gmail.com>
2021-07-10 17:26:51 -0400

    API key hided.

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/storage";
+require('dotenv').config();
+
+console.log(process.env);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBASCZnR-iJWCV4tne73oZSo_HHQvzfxbY",
+  apiKey: process.env.API_KEY,
   authDomain: "my-music-journey.firebaseapp.com",
   databaseURL: "https://my-music-journey.firebaseio.com",
   projectId: "my-music-journey",
   storageBucket: "my-music-journey.appspot.com",
-  messagingSenderId: "218567346610",
-  appId: "1:218567346610:web:39edd366a43dac576a7cab",
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
